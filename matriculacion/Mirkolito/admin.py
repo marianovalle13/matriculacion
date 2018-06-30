8a2a8b86666d43e2e32429fe0461af11ac5a04ee
marianovalle13 <marianovalle13@gmail.com>
2018-06-30 19:17:52 -0300

    clases registradas en admin.py

--- matriculacion/Mirkolito/admin.py
+++ matriculacion/Mirkolito/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+#Modelos ya registradas
+admin.site.register(Persona)
+admin.site.register(Alumno)
+admin.site.register(Profesor)
+admin.site.register(Curso)
+admin.site.register(Nota)
+admin.site.register(MatriculacionAlumno)
+admin.site.register(MatriculacionProfesor)
