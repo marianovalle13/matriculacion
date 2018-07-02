ff93f98613264f9e6ab6e6193798770e387ae499
marianovalle13 <marianovalle13@gmail.com>
2018-07-02 08:17:27 -0300

    actualizado xdxd

--- matriculacion/Mirkolito/views.py
+++ matriculacion/Mirkolito/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+#funcion curso
+def cursos(request):
+	profe = Profesor.objects.get(usuario=request.user)
+	return render(request, "curso.html", {"cursos_del_profe":Curso.objects.filter(id__in=MatriculaProfesor.objects.filter(profesor=profe))})
